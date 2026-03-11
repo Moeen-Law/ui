43f10c324fd4a3c21ed5bb0ab4c3f11358252a3c
Mohamed Medhat <mohamedsheetat@gmail.com>
2026-03-11 04:20:50 +0200

    implement useResetPassword hook

--- src/features/auth/hooks/useResetPassword.ts
+++ src/features/auth/hooks/useResetPassword.ts
@@ -1,7 +1,10 @@
 import { useState } from "react";
 import type { ResetPasswordValues } from "../types";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import api from "@/shared/api";
+import { AxiosError } from "axios";
+import { toast } from "sonner";
+import { errorToastStyle } from "@/shared/constants";
 
 
 
@@ -10,8 +13,7 @@ export const useResetPassword = () => {
     const [isError, setIsError] = useState(false);
     const [isSuccess, setIsSuccess] = useState(false);
     const navigate = useNavigate();
-    const [searchParams] = useSearchParams();
-    const token = searchParams.get("token");
+    
 
 
     const resetPassword = async (data: ResetPasswordValues) => {
@@ -21,12 +23,25 @@ export const useResetPassword = () => {
             setIsSuccess(false);
 
             await api.post("/auth/reset-password", {
-                token,
+                confirmPassword: data.confirmPassword,
                 password: data.password,
             });
-            
-        } catch (error) {
-            
+            navigate("/login");
+        } catch (error: unknown) {
+            if (error instanceof AxiosError) {
+                toast.error(`خطأ اثناء ارسال رابط تغيير كلمة المرور (${error.response?.status})`, { style: errorToastStyle });
+            } else {
+                toast.error("حدث خطأ أثناء تغيير كلمة المرور", { style: errorToastStyle });
+            }
+            return false;
         }
+    };
+
+
+    return {
+        isVerifying,
+        isError,
+        isSuccess,
+        resetPassword,
     };
 }
