ab2b4ec0a8dfbae7975251b9c11a3ccc95bfd1b9
Mohamed Medhat <mohamedsheetat@gmail.com>
2026-03-14 23:43:52 +0200

    tanstack query setup

--- src/App.tsx
+++ src/App.tsx
@@ -10,24 +10,30 @@ import OAuthAuthorize from './features/auth/pages/OAuthAuthorize';
 import NotFound from './shared/pages/NotFound';
 import { Toaster } from "@/components/ui/sonner"
 import useAuthStore from "./features/auth/store/auth";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+
+const queryClient = new QueryClient();
 
 export function App() {
     const { accessToken } = useAuthStore();
     return (
-        <BrowserRouter>
-            <Toaster />
-            <Routes>
-                <Route path="/" element={<Landing />} />
-                <Route path="/chat" element={<Chat />} />
-                <Route path="/signup" element={accessToken ? <Navigate to="/" /> : <SignUp />} />
-                <Route path="/login" element={accessToken ? <Navigate to="/" /> : <Login />} />
-                <Route path="/forgot-password" element={<ForgotPassword />} />
-                <Route path="/reset-password" element={<ResetPassword />} />
-                <Route path="/verify-email" element={<VerifyEmail />} />
-                <Route path="/login/oauth/authorize" element={<OAuthAuthorize />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <Toaster />
+                <Routes>
+                    <Route path="/" element={<Landing />} />
+                    <Route path="/chat" element={<Chat />} />
+                    <Route path="/signup" element={accessToken ? <Navigate to="/" /> : <SignUp />} />
+                    <Route path="/login" element={accessToken ? <Navigate to="/" /> : <Login />} />
+                    <Route path="/forgot-password" element={<ForgotPassword />} />
+                    <Route path="/reset-password" element={<ResetPassword />} />
+                    <Route path="/verify-email" element={<VerifyEmail />} />
+                    <Route path="/login/oauth/authorize" element={<OAuthAuthorize />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </BrowserRouter>
+        </QueryClientProvider>
     );
 }
 
