5a003bf990f22647ed0f67c69882049bcd7fd4b1
Mohamed Medhat <mohamedsheetat@gmail.com>
2026-06-18 01:08:32 +0300

    remove unused imports for ci/cd pipeline

--- src/features/admin/layout/AdminHeader.tsx
+++ src/features/admin/layout/AdminHeader.tsx
@@ -1,4 +1,4 @@
-import { Bell, Home, Menu } from "lucide-react"
+import {  Home, Menu } from "lucide-react"
 import { Link, useLocation } from "react-router-dom"
 import { useTranslation } from "react-i18next"
 import { Button } from "@/components/ui/button"
