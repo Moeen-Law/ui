f0d8c594c3d45169d39b4559b44408795f54fc96
Mohamed Medhat <mohamedsheetat@gmail.com>
2026-06-18 15:47:55 +0300

    fix: clear cache data after user logout

--- src/features/auth/hooks/useLogout.ts
+++ src/features/auth/hooks/useLogout.ts
@@ -7,21 +7,26 @@ import { toast } from "sonner";
 import useAuthStore from "../store/auth";
 import { authService } from "../helpers";
 import i18n from "@/lib/i18n";
+import { useQueryClient } from "@tanstack/react-query";
 
 
 export const useLogout = () => {
     const [loading, setLoading] = useState<boolean>(false);
     const navigate = useNavigate();
     const { removeAccessToken } = useAuthStore();
+    const queryClient = useQueryClient();
 
     const handleLogout = async () => {
         try {
             setLoading(true);
             await api.post(`${authService}/auth/logout`);
-            toast.success(i18n.t("toast.logoutSuccess"), { style: successToastStyle });
+            
             removeAccessToken();
             useAuthStore.persist.clearStorage();
+            
+            queryClient.clear();
             navigate("/");
+            toast.success(i18n.t("toast.logoutSuccess"), { style: successToastStyle });
         } catch (error: unknown) {
             if (error instanceof AxiosError) {
                 toast.error(error.response?.data.message || i18n.t("toast.logoutFailed"), { style: errorToastStyle });
